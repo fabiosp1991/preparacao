62b231921052b2206ed7fbe9c00f2da17ec9121e
fabio <fabiosp1991@hotmail.com>
2023-01-24 19:16:15 -0300

    intial

---
+++ src/test/java/br/com/fabiosp1991/preparacao/PreparacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiosp1991.preparacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreparacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
